b784c9f419963e7b41a274abae8258b35a830b4c
abdelhadi-22 <abdoelbcir33@gmail.com>
2023-12-31 01:32:04 +0100

    adding styles and upload and dynamique creating vectors

--- src/app/api/cmd/route.tsx
+++ src/app/api/cmd/route.tsx
@@ -1,31 +1,57 @@
 // Import necessary modules
 import { NextRequest, NextResponse } from 'next/server';
 import { exec } from 'child_process';
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
+import { pinecone } from "@/libs/pinecone";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
+
+const filePath = "public/docs";
+
 
 // Define your API route handler
 export async function POST(request: NextRequest) {
-  try {
 
-    // Execute the "clear" command (replace with your desired command)
-    exec('npm run ingest', (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error executing command: ${error.message}`);
-        return NextResponse.json({ success: false, error: error.message });
-      }
+    const run = async () => {
+        try {
+            /*load raw docs from the all files in the directory */
+            const directoryLoader = new DirectoryLoader(filePath, {
+                ".pdf": (path) => new PDFLoader(path),
+            });
 
-      if (stderr) {
-        console.error(`Command stderr: ${stderr}`);
-        return NextResponse.json({ success: false, error: stderr });
-      }
+            // const loader = new PDFLoader(filePath);
+            const rawDocs = await directoryLoader.load();
 
-      console.log(`Command stdout: ${stdout}`);
+            /* Split text into chunks */
+            const textSplitter = new RecursiveCharacterTextSplitter({
+                chunkSize: 1000,
+                chunkOverlap: 200,
+            });
 
-      // Send a success response
-      return NextResponse.json({ success: true, stdout });
-    });
+            const docs = await textSplitter.splitDocuments(rawDocs);
+            console.log("split docs", docs);
 
-  } catch (error:any) {
-    console.error(`Error processing request: ${error.message}`);
-    return NextResponse.json({ success: false, error: error.message });
-  }
+            console.log("creating vector store...");
+            /*create and store the embeddings in the vectorStore*/
+            const embeddings = new OpenAIEmbeddings();
+            if (!process.env.PINECONE_INDEX_NAME) {
+                throw new Error("Missing Pinecone index name in .env file");
+            }
+            const index = pinecone.Index(process.env.PINECONE_INDEX_NAME);
+
+            //embed the PDF documents
+            await PineconeStore.fromDocuments(docs, embeddings, {
+                pineconeIndex: index,
+                textKey: "text",
+            });
+        } catch (error) {
+            console.log("error", error);
+            throw new Error("Failed to ingest your data");
+        }
+    };
+
+    run();
+    return NextResponse.json({ success: true })
 }
