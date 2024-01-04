17dfa364d683dc14041e6cc24d9833b18a3fc7e9
youness serrakhi <120403490+YounessSerrakhi@users.noreply.github.com>
2024-01-04 02:59:27 +0100

    add multilanguage support

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -5,15 +5,16 @@ import { ConversationalRetrievalQAChain } from "langchain/chains";
 import { AIMessage, HumanMessage } from "langchain/schema";
 import { pinecone } from "@/libs/pinecone";
 
-const CONDENSE_TEMPLATE = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+const CONDENSE_TEMPLATE = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question with the same language.
 
 Chat History:
 {chat_history}
 Follow Up Input: {question}
 Standalone question:`;
 
-const QA_TEMPLATE = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
+const QA_TEMPLATE = `You are a helpful multilanguage AI assistant. Use the following pieces of context to answer the question at the end.
 If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
+DO respond to the question in question language.
 If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
 
 {context}
@@ -57,7 +58,8 @@ export async function POST(request: Request) {
 				qaTemplate: QA_TEMPLATE,
 				questionGeneratorTemplate: CONDENSE_TEMPLATE,
 				returnSourceDocuments: true,
-				// verbose: true,
+				//verbose: true,
+
 			}
 		);
 
